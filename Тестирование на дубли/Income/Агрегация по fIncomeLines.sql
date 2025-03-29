79c66280a3793ea6d30f708e5ec3df3e5371d23e
KYBqq <kybjob08@gmail.com>
2025-03-29 20:44:20 +0300

    Тестирование данных.
    -После соединения в fIncom, zvezdaFIncomePlusManuals.
    
    В основной сумме, fi_amount, появляются дубли.
    
    На уровне группировки, агрегации через запрос её нет.
    
    Решаю, разбираюсь в этом.

---
+++ Тестирование на дубли/Income/Агрегация по fIncomeLines.sql
@@ -0,0 +1,16 @@
+﻿--SELECT 
+--line_id,
+--item_name, 
+--SUM(amount) as fIncomeLinesAmount,
+--SUM(price) as fIncomeLinesPrice,
+--SUM(quantity) as fIncomeLinesQuantity
+--FROM fIncomeLines
+--WHERE item_name = 'АЕВИТ КАПС. 200МГ №10_МЛГ'
+--GROUP BY line_id, item_name
+--ORDER BY fIncomeLinesQuantity ASC
+
+SELECT 
+SUM(amount) as fIncomeLinesAmount,
+SUM(price) as fIncomeLinesPrice,
+SUM(quantity) as fIncomeLinesQuantity
+FROM fIncomeLines
