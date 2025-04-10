e4128c8d04e4bd20beedba52584de053840b6917
KYBqq <kybjob08@gmail.com>
2025-04-10 08:36:12 +0300

    Тестирование на дубли Receipt
    -Тестирование на дубли в агрегации.

---
+++ Тестирование на дубли/Receipts/Агрегация fReceiptsLines.sql
@@ -0,0 +1,12 @@
+﻿SELECT 
+SUM(quantity) as quantity,
+SUM(pricebase) as pricebase,
+SUM(pricesale) as pricesale,
+SUM(discount) as FIL_discount,
+SUM(amount) as FIL_amount,
+SUM(cogs) as cogs,
+SUM(vat_applied) as vat_applied,
+SUM(vat_amount) as FIL_vat_amount
+FROM fReceiptsLines
+
+
